1b7ec03b30d1bf6daa2f262ca83bccca491d063d
nijimajohn <hirokikotera0211@gmail.com>
2021-09-17 05:54:00 +0000

    [??]

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,14 +8,5 @@ class Member < ApplicationRecord
   has_many :destinations, dependent: :destroy
   has_many :cart_products, dependent: :destroy
   
-  validate :last_name, presence:true
-  validate :first_name, presence:true
-  validate :last_name_kana, presence:true
-  validate :first_name_kana, presence:true
-  validate :address, presence:true
-  validate :postal_code, presence:true
-  validate :telephone_number, presence:true
-  validate :email, presence:true
-  
-  
+
 end
