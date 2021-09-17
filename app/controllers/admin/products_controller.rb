804eca1cc3cc90bd0c95cf381c9ed4ccfaf4ab8e
odajunpei <i.penju1022@gmail.com>
2021-09-17 05:24:01 +0000

    [商品登録機能追加]

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -10,7 +10,6 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-     @product_find = Product.find(params[:id])
     @product.save
     redirect_to admin_products_path, notice: "You have created product successfully."
   end
